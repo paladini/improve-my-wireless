68dff10326f6b07e153587f90c7388dc109dba71
Fernando Paladini <fnpaladini@gmail.com>
2017-09-13 06:21:32 -0300

    Latest commit

---
+++ build/CMakeFiles/com.github.paladini.improvemywireless.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello_world.c"
+  "com.github.paladini.improvemywireless_valac.stamp"
+  "CMakeFiles/com.github.paladini.improvemywireless.dir/src/hello_world.c.o"
+  "com.github.paladini.improvemywireless.pdb"
+  "com.github.paladini.improvemywireless"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.paladini.improvemywireless.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
